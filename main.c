c76218a72a5248366afc235754f4399969994bf8
Elio6174 <elseismylife@gmail.com>
2026-03-29 00:46:56 -0600

    error en schudeleBlock

--- main.c
+++ main.c
@@ -1,52 +1,80 @@
 #include <stdio.h>
 #include <string.h>
 
-void printBlock(char[64]);
+void printBlock(char[]);
 void printBit(char);
 void printBitInt(int);
 void printScheduleBlock(int[64]);
-void schedule(char[]);
+void schedule(char[], int);
+int w(int, int);// revisar esta parte puede ser mas eficiente aun en la
+                // variable n
 
 int main(){
-	char messageBlock[64]= {"hola Mundo"};
-  //printBlock(messageBlock);
+	char messageBlock[55]= {"hola Mundo"};
+  messageBlock[strlen(messageBlock)] = 0x80;
   
-  schedule(messageBlock);
   
-  //printf("prueba de char a int\n");
-  //char cadena[4] = {"asdf"};
-
-  //int data = (int)cadena;
+  schedule(messageBlock, strlen(messageBlock));
+  printBlock(messageBlock);
   
-  //int data;
-  //memcpy(&data, cadena, 4);
-  
-  //printBitInt(data);
   return 0;
 }
 
-void schedule(char block[]){
+
+int w(int row, int n){
+  return (row>>n)|(row<<(32-n)); 
+}
+
+void schedule(char block[], int len){
   int scheduleBlock[64];
+  //block[len+1] = 0x80;
   char temp[4];
 
-  for(int i=0; i<10; i++){
+  for(int i=0; i<64; i++){
     for(int k=0; k<4;k++){
       if(i<16){
         temp[k]=block[4*i+k];
-        //scheduleBlock[i][k]= block[4*i +k];
     }else
         temp[k]=0;
     }
-    //printf("%s\n", temp);i
     scheduleBlock[i]=(temp[0] << 24)|(temp[1] << 16)|(temp[2] << 8)|(temp[3]);
   }
+  scheduleBlock[15] = len*8;
+  //scheduleBlock[11] = 128;
+/*
+  int out1=0, out2=0;
+  for(int i=16; i<18; i++){
+    printf("iteracion: %d\n", i);
+    printf("w1\n");
+    printBitInt(w(scheduleBlock[i-15],18));
 
+    //out1 = w(scheduleBlock[i-15], 7) ^ w(scheduleBlock[i-15], 18) ^ (scheduleBlock[i-15]>>3);
+    //printBitInt(out1);
+    printf("\nw2\n");
+    //out2 = w(scheduleBlock[i-2], 17) ^ w(scheduleBlock[i-2], 19) ^ (scheduleBlock[i-2]>>10);
+    //scheduleBlock[i] = scheduleBlock[i-16] + out1 + scheduleBlock[i-7] + out2;
+
+    //printBitInt(out2);
+    //printf("\nSalida\n");
+    //printBitInt(scheduleBlock[i]);
+    //printf("\n");
+    //printf("\n");
+
+  }
+
+  //printBitInt(w(scheduleBlock[1], 7));
+  //printf("\n");
+  //printBitInt(w(scheduleBlock[1], 18));
+  //printf("\n");
+  //printBitInt(scheduleBlock[1]>>3);
+  //printf("\n");
+*/
   printScheduleBlock(scheduleBlock);
 }
 
 
 void printBlock(char block[]){
-  for(int i = 0; i < 10; i++){
+  for(int i = 0; i < 16; i++){
     for(int k = 0; k < 4; k++){
       printBit(block[4*i + k]);
       printf(" ");
@@ -57,10 +85,11 @@ void printBlock(char block[]){
 }
 
 void printScheduleBlock(int scheduleBlock[64]){
-  for(int i=0; i<10; i++){
+  for(int i=0; i<64; i++){
+    printf("w%d\t  ", i);
     printBitInt(scheduleBlock[i]);
+    printf("\n");
   }
-  printf("\n");
 }
 
 void printBit(char c){
@@ -72,12 +101,10 @@ void printBit(char c){
 
 void printBitInt(int c){
   for (int i = 31; i >= 0; i--) {
-    int k = c >> i; // Desplaza el bit a la posición 0
-    if (k & 1)      // Comprueba si el bit es 1
+    if ((c>>i) & 1)
       printf("1");
     else
       printf("0");
   }
-   printf("\n");
 }
 
