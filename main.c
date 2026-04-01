c3641d80192fe2aafa69728ff84a7f281f95d55d
Elio6174 <elseismylife@gmail.com>
2026-03-31 23:08:57 -0600

    ejecucion dinamica con argumetos

--- main.c
+++ main.c
@@ -3,29 +3,28 @@
 #include <sha256.h>
 #include <stdlib.h>
 
-void printMessage(unsigned char[]);
-
 int main(int argc, char *argv[]){
 
   if(argc < 1){
+    printf("faltan argumentos\n");
     return 1;
   }
   
-  //printf("Mensaje impreso como parametro %s\n", argv[1]);
-	
   unsigned char messageBlock[55];
-  memcpy(messageBlock, argv[1], 55);
   
+
+  int largo = strlen(argv[1]);
+  if(largo > 55){
+    printf("mensaje mayor a 55 bytes se recortara\n");
+    largo = 55;
+  }
+
+  memset(messageBlock, 0, 55);
+  memcpy(messageBlock, argv[1], largo);
   //printMessage(messageBlock);
 
-  for(int k = 0; k < 55; k++){
-    printf("%c", messageBlock[k]);
-  }
-  printf("\n");
 
-
-
-
+  printf("%s\n",messageBlock);
   unsigned char *sha256Out = sha256(messageBlock);
 
   for(int i = 0; i < 32; i++) {
@@ -34,12 +33,3 @@ int main(int argc, char *argv[]){
   printf("\n");
   return 0;
 }
-
-void printMessage(unsigned char message[]){
-  for(int i = 0; i< 55; i++){
-    printf("%s", message[i]);
-  }
-  printf("\n");
-}
-
-
