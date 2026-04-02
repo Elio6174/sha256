b87d96f5d3f996e13959d2222f858c90c06f1578
Elio6174 <elseismylife@gmail.com>
2026-04-01 21:03:23 -0600

    version final con parametros a archivos

--- main.c
+++ main.c
@@ -5,16 +5,42 @@
 
 int main(int argc, char *argv[]){
 
-  if(argc < 1){
+  if(argc < 2){
     printf("faltan argumentos\n");
     return 1;
   }
+
+  unsigned char *sha256Out;
+
+  if(strcmp(argv[1], "-m") == 0 && argc>2){
+    sha256Out = sha256(argv[2]);
+  }else{
+    FILE *archivo;
+    archivo = fopen(argv[1], "rb");
     
-  unsigned char *sha256Out = sha256(argv[1]);
+    if (archivo != NULL) {
+      fseek(archivo, 0, SEEK_END);
+      long tam = ftell(archivo); 
+      fseek(archivo, 0, SEEK_SET);
+      
+      unsigned char* data = (unsigned char*)calloc(tam + 1, sizeof(unsigned char));
+      if(!data) return 1;
+
+      size_t leidos = fread(data, 1, tam, archivo);
+      fclose(archivo);
+      sha256Out = sha256(data);
+    }
+  }
+
 
   for(int i = 0; i < 32; i++) {
     printf("%02x", sha256Out[i]);
   }
   printf("\n");
+
+
+
+
+
   return 0;
 }
